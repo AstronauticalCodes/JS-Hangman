549201f99ac39de926a709d1a296119e84271c81
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-11-16 00:51:45 +0530

    Completed Stage-3.

--- stage-3-makeYourChoice.js
+++ stage-3-makeYourChoice.js
@@ -1,11 +1,14 @@
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
-const input = require('sync-input')
+// const input = require('sync-input')
 
 const user = input("H A N G M A N\nThe game will be available soon.");
 
-if (user === "python") {
+const words = ["python", "java", "swift", "javascript"];
+const randomWord = words[Math.floor(Math.random() * words.length)];
+
+if (user === randomWord) {
     console.log("You survived!");
 } else {
     console.log("You lost!");
