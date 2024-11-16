3d3ea88959ef3282d27ca83f52a51cc3391f0533
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-11-16 09:32:50 +0530

    Made stage-3 compatible for Hyperskill's input method.

--- stage-3-makeYourChoice.js
+++ stage-3-makeYourChoice.js
@@ -1,14 +1,14 @@
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
-// const input = require('sync-input')
+const input = require('sync-input');
 
 const user = input("H A N G M A N\nThe game will be available soon.");
 
 const words = ["python", "java", "swift", "javascript"];
-const randomWord = words[Math.floor(Math.random() * words.length)];
+const guessWord = words[Math.floor(Math.random() * words.length)];
 
-if (user === randomWord) {
+if (user === guessWord) {
     console.log("You survived!");
 } else {
     console.log("You lost!");
